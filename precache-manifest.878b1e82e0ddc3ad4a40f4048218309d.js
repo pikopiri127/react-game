4c0cdc18b4bda00a36cd3d8a4fd05ba9ed853e4c
pikopiri127 <hb.ucfbootcamp@gmail.com>
2019-01-13 12:45:27 -0500

    Updates

---
+++ precache-manifest.878b1e82e0ddc3ad4a40f4048218309d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cda9c7c5162c773fb401",
+    "url": "/react-game/static/js/runtime~main.cda9c7c5.js"
+  },
+  {
+    "revision": "a7fce0253ac988631a4c",
+    "url": "/react-game/static/js/main.a7fce025.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a7fce0253ac988631a4c",
+    "url": "/react-game/static/css/main.52defad5.chunk.css"
+  },
+  {
+    "revision": "4f61e15f3d57f91f2d7a61dd7fcfc1ee",
+    "url": "/react-game/index.html"
+  }
+];
